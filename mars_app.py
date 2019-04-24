b7baa37ef95a234c8bbf2184029c34ce47dece64
ndibiase <46508096+ndibiase@users.noreply.github.com>
2019-04-23 23:08:57 -0700

    Add files via upload

--- mars_app.py
+++ mars_app.py
@@ -3,3 +3,15 @@ from flask import Flask, render_template, jsonify, redirect
 import pymongo
 import scrape_mars
 import os
+
+app = Flask(__name__)
+
+
+client = pymongo.MongoClient()
+db = client.Mars_Database
+collection = db.Mars_Data
+
+@app.route("/scrape")
+def scrape()
+
+
